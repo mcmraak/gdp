7d4dfd1fe05288701a642153cc25ef55ef6d6e13
Alexander Ablizin <zen@8ber.ru>
2021-03-16 12:51:04 +0400

    fix

--- Plugin.php
+++ Plugin.php
@@ -6,6 +6,9 @@ class Plugin extends PluginBase
 {
     public function registerComponents()
     {
+        return [
+            'Zen\Gdp\Components\Doc' => 'doc',
+        ];
     }
 
     public function registerSettings()
